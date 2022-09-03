9e87f8298b6073581792d966db782f546b0b1b3c
Arman-Kabir <armankabir8@gmail.com>
2022-09-03 12:16:09 +0600

    get post done

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,18 +1,35 @@
 
+const tools = [
+    { id: 1, name: "Hammer1" },
+    { id: 2, name: "Hammer2" },
+    { id: 3, name: "Hammer3" }
+];
 
 module.exports.getAllTools = (req, res, next) => {
+    const { limit, page } = req.query;
+    console.log(limit, page);
     // res.send("tools found");
     // res.download(__dirname+ "/tools.controller.js ");
     // const {ip,query,params,body,headers} = req;
     // console.log(ip,query,params,body,headers);
-    res.send("tools found getAllTools ");
+    // res.send(tools);
+    res.json(tools.slice(0, limit));
 
 }
 
 module.exports.saveATool = (req, res, next) => {
-    res.send("tool added");
+    // console.log(req.body);
+    console.log(req.query);
+    tools.push(req.body);
+    res.send(tools);
+    
 }
 
 module.exports.getToolDetail = (req, res, next) => {
-    res.send("Tool detail found");
+    const { id } = req.params;
+    console.log(id);
+    // const filter = { _id: id };
+    const foundTool = tools.find(tool => tool.id === Number(id));
+    res.send(foundTool);
+
 }
